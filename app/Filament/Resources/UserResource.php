3e9262fcd8a495917801ee2cbfce746a7de8c5a8
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2025-01-26 19:26:10 -0600

    feat: :sparkles: Añadiendo etiqueta de navegación para el recurso de empleados

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -19,6 +19,8 @@ class UserResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
+    protected static ?string $navigationLabel = 'Employees';
+
     public static function form(Form $form): Form
     {
         return $form
