b8f01fce9da40f8a9970ca484aa6b697ab6cc6b9
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2025-02-07 18:49:41 -0600

    feat: :sparkles: Añadiendo el recurso de hoja de tiempo con formularios y páginas para crear, editar y listar

---
+++ app/Filament/Resources/TimesheetResource.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TimesheetResource\Pages;
+use App\Filament\Resources\TimesheetResource\RelationManagers;
+use App\Models\Timesheet;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class TimesheetResource extends Resource
+{
+    protected static ?string $model = Timesheet::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Select::make('user_id')
+                    ->relationship(name: 'user', titleAttribute: 'name')
+                    ->required(),
+                Forms\Components\Select::make('calendar_id')
+                    ->relationship(name: 'calendar', titleAttribute: 'name')
+                    ->required(),
+                Forms\Components\Select::make('type')
+                    ->options([
+                        'work' => 'Working',
+                        'pause' => 'In Pause',
+                    ])
+                    ->required(),
+                Forms\Components\DateTimePicker::make('day_in')
+                    ->native(false)
+                    ->required(),
+                Forms\Components\DateTimePicker::make('day_out')
+                    ->native(false)
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('user.name')
+                    ->numeric()
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('calendar.name')
+                    ->numeric()
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('type')
+                    ->badge()
+                    ->color(fn(string $state): string => match ($state) {
+                        'work' => 'success',
+                        'pause' => 'warning',
+                    })
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('day_in')
+                    ->dateTime()
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('day_out')
+                    ->dateTime()
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                SelectFilter::make('type')
+                    ->options([
+                        'work' => 'Working',
+                        'pause' => 'In Pause',
+                    ]),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTimesheets::route('/'),
+            'create' => Pages\CreateTimesheet::route('/create'),
+            'edit' => Pages\EditTimesheet::route('/{record}/edit'),
+        ];
+    }
+}
