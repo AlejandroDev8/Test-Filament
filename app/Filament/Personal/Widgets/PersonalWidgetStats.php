18103bc0191196d89d147fc019233cde6728f767
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2025-02-09 15:26:08 -0600

    feat: :sparkles: Adding total work hours calculation to PersonalWidgetStats

--- app/Filament/Personal/Widgets/PersonalWidgetStats.php
+++ app/Filament/Personal/Widgets/PersonalWidgetStats.php
@@ -2,11 +2,14 @@
 
 namespace App\Filament\Personal\Widgets;
 
+use Carbon\Carbon;
 use App\Models\User;
 use App\Models\Holiday;
+
+use App\Models\Timesheet;
+
 use function Pest\Laravel\get;
 use Illuminate\Support\Facades\Auth;
-
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 
@@ -26,6 +29,23 @@ class PersonalWidgetStats extends BaseWidget
         return $totalApprovedHolidays;
     }
 
+    protected function getTotalWork(User $user)
+    {
+        $timeSheets  = Timesheet::where('user_id', $user->id)
+            ->where('type', 'work')->get();
+        $sumHours = 0;
+
+        foreach ($timeSheets as $timeSheet) {
+            $startTime = Carbon::parse($timeSheet->day_in);
+            $endTime = Carbon::parse($timeSheet->day_out);
+
+            $hours = $startTime->diffInHours($endTime);
+            $sumHours += $hours;
+        }
+
+        return $sumHours;
+    }
+
     protected function getStats(): array
     {
         return [
@@ -33,6 +53,8 @@ class PersonalWidgetStats extends BaseWidget
                 ->icon('heroicon-o-clock'),
             Stat::make('Approved Holidays', $this->getApprovedHolidays(Auth::user()))
                 ->icon('heroicon-o-check-circle'),
+            Stat::make('Total Work', $this->getTotalWork(Auth::user()))
+                ->icon('heroicon-o-calendar'),
         ];
     }
 }
