29a46b8e01c22eb9b425b61fcc028d4777e32f96
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2025-02-09 17:36:28 -0600

    feat: :sparkles: Adding 'In Work' and 'In Pause' actions to Timesheet management

--- app/Filament/Personal/Resources/TimesheetResource/Pages/ListTimesheets.php
+++ app/Filament/Personal/Resources/TimesheetResource/Pages/ListTimesheets.php
@@ -2,9 +2,13 @@
 
 namespace App\Filament\Personal\Resources\TimesheetResource\Pages;
 
-use App\Filament\Personal\Resources\TimesheetResource;
 use Filament\Actions;
+use Filament\Actions\Action;
+use Illuminate\Support\Facades\Auth;
 use Filament\Resources\Pages\ListRecords;
+use App\Filament\Personal\Resources\TimesheetResource;
+use App\Models\Timesheet;
+use Carbon\Carbon;
 
 class ListTimesheets extends ListRecords
 {
@@ -13,6 +17,24 @@ class ListTimesheets extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
+            Action::make('In Work')
+                ->color('success')
+                ->label('Entrar a trabajar')
+                ->requiresConfirmation()
+                ->action(function () {
+                    $user = Auth::user();
+                    $timeSheet = new Timesheet();
+                    $timeSheet->user_id = $user->id;
+                    $timeSheet->calendar_id = 1;
+                    $timeSheet->type = 'work';
+                    $timeSheet->day_in = Carbon::now();
+                    $timeSheet->day_out = Carbon::now();
+                    $timeSheet->save();
+                }),
+            Action::make('In Pause')
+                ->color('info')
+                ->label('Tomar un descanso')
+                ->requiresConfirmation(),
             Actions\CreateAction::make(),
         ];
     }
